435326fd8a985097df1ecd20921a797d6403cc2b
hny03 <hannyabg@gmail.com>
2021-01-11 21:40:44 -0300

    sala

---
+++ Assets/endOfLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class endOfLevel : MonoBehaviour
+{    public void EndOfGame()
+    {
+        //Debug.Log("HIT");
+        //Application.Quit();
+        SceneManager.LoadScene("Sala");
+    }
+}
